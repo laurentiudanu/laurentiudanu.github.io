2bd800e1e25996395584ac168b8db09441bbec43
Laurentiu Danu <laurentiu.danu@gmail.com>
2017-04-26 21:46:54 +0300

    ELAN proposal #1

---
+++ elan/js/functions.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+  (function($){
+    function removeIA() {
+      $(".index-animation").addClass("fadeOut").bind('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',   function(){ 
+        $(this).remove();
+      });
+    }
+    $(".index-animation h1").addClass("delay-800, flash");
+    $(".index-animation h1").bind('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){ 
+      $(this).remove();
+      $(".index-animation .ia-1").addClass("bounceIn").bind('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',   function(){ 
+            $(".index-animation .ia-2").addClass("fadeInUp").bind('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',   function(){ 
+              window.setTimeout(removeIA, 900)
+            });
+      });
+    });
+
+
+  })(jQuery);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  function is_touch_device() {
+    return (('ontouchstart' in window)
+        || (navigator.MaxTouchPoints > 0)
+        || (navigator.msMaxTouchPoints > 0));
+    }
+  var pHtml = document.getElementsByTagName( 'html' )[0];
+  if (!is_touch_device()) {
+    pHtml.setAttribute( 'class', 'no-touch' );
+  }
+  else {
+    pHtml.setAttribute( 'class', 'touch' );
+  }
+
+});
